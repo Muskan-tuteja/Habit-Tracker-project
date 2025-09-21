c57d7d5791ea1366f47b514a95d0b624681d49b9
muskan <muskantuteja714@gmail.com>
2025-09-21 15:34:13 +0530

    Prepare for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import './output.css';
-
+import { HashRouter } from "react-router-dom"; // ✅ Add this
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter> {/* ✅ Wrap App with HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
